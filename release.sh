12a300083640c18fe32c1b617c6ebebf3602f3dc
Jérémy Viès <4450519+jvies@users.noreply.github.com>
2026-09-23 08:54:05 +0200

    fix release for beta/latest tagging

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 src_image_base=$1
 dst_image=$2
+other_tag=$3
 variants=("allinone" "nginx" "phponly")
 
 function publish_image() {
@@ -32,14 +33,7 @@ MINOR_VERSION=$(echo "$HUMHUB_VERSION" | cut -d'.' -f2)
 HUMHUB_MACRO_VERSION=${MAJOR_VERSION}.${MINOR_VERSION}
 
 # publish the image under the minor-version pattern
-publish_image "${HUMHUB_VERSION}" "${HUMHUB_MACRO_VERSION}"
-
-# publish the image according to the branch name
-for branch in $(git branch --contains)
-do
-    [[ "$branch" == "beta" ]] && publish_image "${HUMHUB_VERSION}" "beta"
-    [[ "$branch" == "main" ]] && publish_image "${HUMHUB_VERSION}" "latest"
-done
+publish_image "${HUMHUB_VERSION}" "${HUMHUB_MACRO_VERSION}" ${other_tag}
 
 # ensure a 0 exit code if we get here
 exit 0
